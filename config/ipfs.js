6d7da9997d15dd57d16435bc455d07b5b199c84b
mbcse <mbcse50@gmail.com>
2021-07-01 19:14:15 +0530

    Dashboard public files error fixed

--- config/ipfs.js
+++ config/ipfs.js
@@ -1,5 +1,5 @@
 const ipfsAPI = require("ipfs-api");
-const ipfs = ipfsAPI("localhost", "5001", { protocol: "http" });
+const ipfs = ipfsAPI("https://ipfs.io", "5001", { protocol: "https" });
 const fs=require("fs");
 
 module.exports={
